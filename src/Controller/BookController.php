bfde39763afbf77c7ea9f486e44b1828d66fa2b2
Your Name <you@example.com>
2023-01-16 19:46:59 +0000

    API-sf-psg add serializer and param converter, add detail book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,18 +3,27 @@
 namespace App\Controller;
 
 use App\Repository\BookRepository;
+use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class BookController extends AbstractController
 {
     #[Route('api/books', name: 'app_book', methods: ['GET'])]
-    public function getBookList(BookRepository $bookRepository): JsonResponse
+    public function getBookList(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
         $books = $bookRepository->findAll();
-        return new JsonResponse([
-            'books' => $books,
-        ]);
+        $jsonBooks = $serializer->serialize($books, 'json');
+        return new JsonResponse($jsonBooks, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('api/books/{id}', name: 'app_book', methods: ['GET'])]
+    public function getDetailBook(Book $book, SerializerInterface $serializer)
+    {
+        $jsonBook = $serializer->serialize($book, 'json');
+        return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
     }
 }
